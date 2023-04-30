c6445d3d27eabc3d6b650e69067f7559096e4d58
Rume7 <rhume.disi@turntabl.io>
2023-04-30 13:01:19 +0100

    Implemented unit test for the Meal class.

--- src/test/java/io/turntabl/meal/MealTest.java
+++ src/test/java/io/turntabl/meal/MealTest.java
@@ -2,13 +2,25 @@ package io.turntabl.meal;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class MealTest {
 
-    //Use mockito
+    @Test
+    void testServeMealForHuman() {
+        Meal meal = mock(Meal.class);
+        String humanMeal = "Human meal served";
+        when(meal.serveMeal()).thenReturn(humanMeal);
+        assertEquals(meal.serveMeal(), humanMeal);
+    }
 
     @Test
-    void serveMeal() {
+    void testServeMealForMartian() {
+        Meal meal = mock(Meal.class);
+        String martianMeal = "Martian meal served";
+        when(meal.serveMeal()).thenReturn(martianMeal);
+        assertEquals(meal.serveMeal(), martianMeal);
     }
 }
