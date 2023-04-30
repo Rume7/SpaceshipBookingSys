595304bb58e36a9c46cb5dcc4bf32a67ca97c40d
RhumeLeap <rhume.disi@turntabl.io>
2023-04-30 03:06:42 +0100

    Cleaned up white spaces

--- src/main/java/io/turntabl/belief/Belief.java
+++ src/main/java/io/turntabl/belief/Belief.java
@@ -4,5 +4,4 @@ public interface Belief {
 
     String plutoBelief();
 
-
 }
--- src/main/java/io/turntabl/belief/HumanBelief.java
+++ src/main/java/io/turntabl/belief/HumanBelief.java
@@ -2,7 +2,6 @@ package io.turntabl.belief;
 
 public class HumanBelief implements Belief {
 
-
     @Override
     public String plutoBelief() {
         return "Pluto is a Moon";
