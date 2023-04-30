d2cc1226c39ca667bca2d0df3d0e27d55951bd68
RhumeLeap <rhume.disi@turntabl.io>
2023-04-30 03:35:13 +0100

    Class housekeeping.

--- src/main/java/io/turntabl/seat/HumanSeat.java
+++ src/main/java/io/turntabl/seat/HumanSeat.java
@@ -1,12 +1,9 @@
 package io.turntabl.seat;
 
-import io.turntabl.meal.HumanMeal;
-
 public class HumanSeat extends Seat {
 
     public HumanSeat(String ID, String name) {
         super(ID, name);
-        this.meal = new HumanMeal();
     }
 
     public SeatType getSeatType() {
--- src/main/java/io/turntabl/seat/MartianSeat.java
+++ src/main/java/io/turntabl/seat/MartianSeat.java
@@ -1,12 +1,9 @@
 package io.turntabl.seat;
 
-import io.turntabl.meal.MartianMeal;
-
 public class MartianSeat extends Seat {
 
     public MartianSeat(String ID, String name) {
         super(ID, name);
-        this.meal = new MartianMeal();
     }
 
     @Override
